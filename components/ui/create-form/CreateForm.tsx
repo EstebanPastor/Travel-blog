a37f4186ea970b24c0b703d8a1e1bc807d7262f2
Esteban Pastor <estebanandrespastor@gmail.com>
2023-12-22 07:59:57 -0300

    feat: :sparkles: Add create post form with validations schemas and image upload
    Now the user can upload posts and the image is stored in mongo db and edge store cloud, also the user can see a preview of the image

--- components/ui/create-form/CreateForm.tsx
+++ components/ui/create-form/CreateForm.tsx
@@ -9,7 +9,10 @@ import { useEdgeStore } from "@/lib/edgestore";
 import { useEffect, useState } from "react";
 import { SingleImageDropzone } from "../single-image-dragzone/SingleImageDragzone";
 
-const CreateForm = ({ user }) => {
+import { userTypes } from "@/types/userTypes";
+import { createPost } from "@/app/actions/blogActions";
+
+const CreateForm = ({ user }: { user: userTypes }) => {
   const [file, setFile] = useState<File>();
   const [imagePath, setImagePath] = useState("");
   const { edgestore } = useEdgeStore();
@@ -35,13 +38,59 @@ const CreateForm = ({ user }) => {
         <h1 className="text-center text-2xl font-extrabold mb-10">
           Create a Post
         </h1>
-        {
-          !user ? (<h2 className="text-center text-xl font-extrabold uppercase">Please sign up or log in to create a post!</h2>) : (
-            <>
-            <SingleImageDropzone />
-            </>
-          )
-        }
+        {!user ? (
+          <h2 className="text-center text-xl font-extrabold uppercase">
+            Please sign up or log in to create a post!
+          </h2>
+        ) : (
+          <>
+            <SingleImageDropzone 
+            onChange={(file) => setFile(file)}
+            value={file}
+            width={200}
+            height={200}
+            />
+            <Form
+              action={createPost}
+              className="flex flex-col gap-5 mt-5"
+              onSubmit={() => setFile(undefined)}
+            >
+              <Input type="hidden" name="image" value={imagePath} />
+              <Input name="title" type="text" placeholder="Enter title" />
+              <textarea
+                required
+                name="description"
+                rows={10}
+                placeholder="Write here..."
+                className="text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-primary sm:text-sm sm:leading-6 border w-full border-gray-200 p-2 rounded-md py-1.5"
+              ></textarea>
+              <select
+                name="category"
+                required
+                className="text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-primary sm:text-sm sm:leading-6 border w-full border-gray-200 p-2 rounded-md py-1.5"
+              >
+                <option value="" disabled selected>
+                  Choose Tag
+                </option>
+                <option value="Adventure">Adventure</option>
+                <option value="Culture">Culture</option>
+                <option value="Journey">Journey</option>
+                <option value="Discovery">Discovery</option>
+                <option value="Wanderlust">Wanderlust</option>
+              </select>
+              <Input 
+              name="email"
+              type="hidden"
+              value={user?.email || ""}
+              />
+              <Button 
+              type="submit"
+              text="Create"
+              aria="create blog"
+              />
+            </Form>
+          </>
+        )}
       </div>
     </div>
   );
